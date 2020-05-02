e56b5fc5503d04049a7f13f9f3d6f5c424cd1569
Nick <Nick@DESKTOP-CFF9TFM>
2020-05-02 12:24:18 -0500

    backup

--- Movies/Pages/Index.cshtml.cs
+++ Movies/Pages/Index.cshtml.cs
@@ -19,13 +19,13 @@ namespace Movies.Pages
         /// The current search terms 
         /// </summary>
         [BindProperty]
-        public string SearchTerms { get; set; } = "";
+        public string SearchTerms { get; set; } 
 
         /// <summary>
         /// The filtered MPAA Ratings
         /// </summary>
         [BindProperty]
-        public string[] MPAARatings { get; set; }
+        public string[] MPAARatings { get; set; } 
 
         /// <summary>
         /// The filtered genres
@@ -55,6 +55,7 @@ namespace Movies.Pages
         /// </summary>
     public void OnGet(double? IMDBMin, double? IMDBMax, double? RottenMin, double? RottenMax)
         {
+            
             this.IMDBMin = IMDBMin;
             this.IMDBMax = IMDBMax;
             this.RottenMin = RottenMin;
@@ -64,11 +65,92 @@ namespace Movies.Pages
             MPAARatings = Request.Query["MPAARatings"];
             Genres = Request.Query["Genres"];
 
+            /*
             Movies = MovieDatabase.Search(SearchTerms);
             Movies = MovieDatabase.FilterByMPAARating(Movies, MPAARatings);
             Movies = MovieDatabase.FilterByGenre(Movies, Genres);
             Movies = MovieDatabase.FilterByIMDBRating(Movies, IMDBMin, IMDBMax);
             Movies = MovieDatabase.FilterByRottenRating(Movies, RottenMin, RottenMax);
+            */
+
+            Movies = MovieDatabase.All;
+
+            if(SearchTerms != null)
+            {
+                  Movies = Movies.Where(movie => 
+                  movie.Title != null && 
+                  movie.Title.Contains(SearchTerms, StringComparison.CurrentCultureIgnoreCase)
+                  );
+            //    Movies = from movie in Movies
+            //             where movie.Title != null && movie.Title.Contains(SearchTerms, StringComparison.CurrentCultureIgnoreCase)
+            //             select movie;
+            }
+
+            if(MPAARatings != null && MPAARatings.Length != 0)
+            {
+                Movies = Movies.Where(movie => 
+                movie.MPAARating != null && 
+                MPAARatings.Contains(movie.MPAARating)
+                );
+            }
+
+            if(Genres != null && Genres.Count() != 0)
+            {
+                Movies = Movies.Where(movie =>
+                movie.MajorGenre != null &&
+                Genres.Contains(movie.MajorGenre));
+            }
+            
+            if(IMDBMin != null || IMDBMax != null)
+            {
+
+                if(IMDBMin == null)
+                {
+                    Movies = Movies.Where(movie =>
+                    movie.IMDBRating <= IMDBMax
+                    );
+                }
+
+                else if(IMDBMax == null)
+                {
+                    Movies = Movies.Where(movie =>
+                    movie.IMDBRating >= IMDBMin
+                    );
+                }
+
+                else
+                {
+                    Movies = Movies.Where(movie =>
+                    movie.IMDBRating >= IMDBMin && movie.IMDBRating <= IMDBMax
+                    );
+                }
+                
+            }
+
+            if(RottenMin != null || RottenMax != null)
+            {
+                if(RottenMin == null)
+                {
+                    Movies = Movies.Where(movie =>
+                    movie.RottenTomatoesRating <= RottenMax
+                    );
+                }
+                else if(RottenMax == null)
+                {
+                    Movies = Movies.Where(movie =>
+                    movie.RottenTomatoesRating >= RottenMin
+                    );
+                }
+                else
+                {
+                    Movies = Movies.Where(movie =>
+                    movie.RottenTomatoesRating >= RottenMin &&
+                    movie.RottenTomatoesRating <= RottenMax
+                    );
+                }
+            }
+
+
         }
 
 
